9dbc16bfde44c6c309e94c373d47627098263fff
KhohZongEu <khohzongeu@example.com>
2024-08-11 14:09:35 +0800

    Connect ultra-cap.js to Firebase

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
- import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+
 
 let hydrogen_temp = 0;
 let hydrogen_volt = 0;
@@ -19,31 +20,62 @@ let battery_amp = 0;
 
 
 const firebaseConfig = {
-    apiKey: "process.env.APIKEY",
-    authDomain: "process.env.AUTHDOMAIN",
-    databaseURL: "process.env.DATABASEURL",
-    projectId: "process.env.PROJECTID",
-    storageBucket: "process.env.STORAGEBUCKET",
-    messagingSenderId: "process.env.MESSAGINGSENDERID",
-    appId: "process.env.APPID",
-    measurementId: "process.env.MEASUREMENTID"
-};
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DB_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGE_Sender_ID",
+    appId: "YOUR_APP_ID",
+    measurementId: "YOUR_MEASUREMENT_ID",
+  };
 
 const app = initializeApp(firebaseConfig);
- const database = getDatabase(app);
+const database = getDatabase(app);
 
-document.getElementById("hydrogen-temp").innerHTML = hydrogen_temp + ' °C';
-document.getElementById("hydrogen-volt").innerHTML = hydrogen_volt + ' V';
-document.getElementById("hydrogen-amp").innerHTML = hydrogen_amp + ' A';
+onValue(ref(database,'CANBUS/hydrogen'), (snapshot) => {
+  hydrogen_temp = snapshot.val().temp;
+  hydrogen_volt = snapshot.val().volt;
+  hydrogen_amp = snapshot.val().amp;
 
-document.getElementById("fuel-cell-temp").innerHTML = fuel_cell_temp + ' °C';
-document.getElementById("fuel-cell-volt").innerHTML = fuel_cell_volt + ' V';
-document.getElementById("fuel-cell-amp").innerHTML = fuel_cell_amp + ' A';
+  document.getElementById("hydrogen-temp").innerHTML = hydrogen_temp + ' °C';
+  document.getElementById("hydrogen-volt").innerHTML = hydrogen_volt + ' V';
+  document.getElementById("hydrogen-amp").innerHTML = hydrogen_amp + ' A';
+});
 
-document.getElementById("ultra-cap-temp").innerHTML = ultra_cap_temp + ' °C';
-document.getElementById("ultra-cap-volt").innerHTML = ultra_cap_volt + ' V';
-document.getElementById("ultra-cap-amp").innerHTML = ultra_cap_amp + ' A';
 
-document.getElementById("battery-temp").innerHTML = battery_temp + ' °C';
-document.getElementById("battery-volt").innerHTML = battery_volt + ' V';
-document.getElementById("battery-amp").innerHTML = battery_amp + ' A';
+onValue(ref(database,'CANBUS/fuel_cell'), (snapshot) => {
+  fuel_cell_temp = snapshot.val().temp;
+  fuel_cell_volt = snapshot.val().volt;
+  fuel_cell_amp = snapshot.val().amp;
+
+  document.getElementById("fuel-cell-temp").innerHTML = fuel_cell_temp + ' °C';
+  document.getElementById("fuel-cell-volt").innerHTML = fuel_cell_volt + ' V';
+  document.getElementById("fuel-cell-amp").innerHTML = fuel_cell_amp + ' A';
+});  
+
+onValue(ref(database,'CANBUS/ultra_cap'), (snapshot) => {
+  ultra_cap_temp = snapshot.val().temp;
+  ultra_cap_amp = snapshot.val().amp;
+  let tmp = snapshot.val().volt;
+  for (let i = 0; i < tmp.length; i++) {
+    ultra_cap_volt += tmp[i];
+  }
+
+  document.getElementById("ultra-cap-temp").innerHTML = ultra_cap_temp + ' °C';
+  document.getElementById("ultra-cap-volt").innerHTML = ultra_cap_volt + ' V';
+  document.getElementById("ultra-cap-amp").innerHTML = ultra_cap_amp + ' A';
+
+});  
+
+onValue(ref(database,'CANBUS/battery'), (snapshot) => {
+  battery_temp = snapshot.val().temp;
+  battery_volt = snapshot.val().volt;
+  battery_amp = snapshot.val().amp;
+
+  document.getElementById("battery-temp").innerHTML = battery_temp + ' °C';
+  document.getElementById("battery-volt").innerHTML = battery_volt + ' V';
+  document.getElementById("battery-amp").innerHTML = battery_amp + ' A';
+});
+
+export default database;
--- js/pages/ultra-cap.js
+++ js/pages/ultra-cap.js
@@ -1,3 +1,6 @@
+import database from '../main.js'
+import {ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+
 //-------------------------- color changing variables --------------------------------
 const lowVolt = 0;
 const highVolt = 3;
@@ -6,13 +9,13 @@ const redColorHigh = 0;
 const greenColorLow = 0;
 const greenColorHigh = 255;
 
+
 function changeColor(id, Voltage) {
     let greenVal = greenColorLow + (greenColorHigh - greenColorLow) * (Voltage - lowVolt) / (highVolt - lowVolt);
     let redVal = redColorLow + (redColorHigh - redColorLow) * (Voltage - lowVolt) / (highVolt - lowVolt);;
     let newColor = "rgba(" + redVal + ", " + greenVal + ", 0)"
     document.getElementById(id).style.backgroundColor = newColor;
 }
-
 function createGrid() {
     let totalCell = 0;
     for (let i = 0; i < 6; i++) {
@@ -27,5 +30,24 @@ function createGrid() {
 }
 
 createGrid();
-changeColor("cell1", 0.4);
-document.getElementById("avr-ultra-cap-volt").innerHTML = 0 + ' V';
+
+onValue(ref(database,'CANBUS/ultra_cap'), (snapshot) => {
+    let ultra_cap_temp = snapshot.val().temp;
+    let avr_ultra_cap_volt = 0;
+    let ultra_cap_amp = snapshot.val().amp;
+    let tmp = snapshot.val().volt;
+
+
+    for (let i = 0; i < tmp.length; i++) {
+      avr_ultra_cap_volt += tmp[i];
+      let cellName = 'cell' + (i + 1);
+      changeColor(cellName, tmp[i]);
+    }
+
+    avr_ultra_cap_volt /= tmp.length;
+
+    document.getElementById("avr-ultra-cap-temp").innerHTML = ultra_cap_temp + ' °C';
+    document.getElementById("avr-ultra-cap-volt").innerHTML = avr_ultra_cap_volt + ' V';
+    document.getElementById("avr-ultra-cap-amp").innerHTML = ultra_cap_amp + ' A';
+
+  });  
